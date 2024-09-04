54b393e39a3f3cef20aed7215b726571cf1266b4
french-femboi <cat@french-femboi.eu>
2024-09-04 20:34:56 +0200

    chore: Uncomment app bar in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -71,16 +71,16 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-//      appBar: AppBar(
-//        backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
-//        title: Text(
-//          widget.title,
-//          style: TextStyle(
-//            fontWeight: FontWeight.bold,
-//            color: Theme.of(context).colorScheme.primary,
-//          ),
-//        ),
-//      ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).colorScheme.surface,
+          ),
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
